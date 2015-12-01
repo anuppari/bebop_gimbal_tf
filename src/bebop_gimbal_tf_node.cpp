6d7519ddceaec6612077c4c688efa0d0f4b84c8f
Anup <anuppari@ufl.edu>
2015-12-01 17:29:33 -0500

    Working prototype

--- src/bebop_gimbal_tf_node.cpp
+++ src/bebop_gimbal_tf_node.cpp
@@ -11,6 +11,8 @@ private:
 	ros::NodeHandle nh_;
 	tf::TransformBroadcaster br_;
 	tf::Transform staticTransform_;
+	ros::Subscriber gimbalSub_;
+	ros::Subscriber mocapSub_;
 	std::string bebopName_;
 	std::string bebopImageName_;
 	int pan_; // yaw
@@ -24,17 +26,17 @@ public:
 		nh_.param<std::string>("bebop_image_name", bebopImageName_, "bebop_image");
 		
 		// Bebop camera gimbal state subscriber
-		nh_.subscribe("bebop/states/ARDrone3/CameraState/Orientation",10,&bebop_gimbal_tf::camStateCB,this);
+		gimbalSub_ = nh_.subscribe("bebop/states/ARDrone3/CameraState/Orientation",10,&bebop_gimbal_tf::camStateCB,this);
 		
 		// Mocap subscriber
-		nh_.subscribe("pose",10,&bebop_gimbal_tf::poseCB,this);
+		mocapSub_ = nh_.subscribe(bebopName_+"/pose",10,&bebop_gimbal_tf::poseCB,this);
 	}
 	
 	void poseCB(const geometry_msgs::PoseStampedConstPtr& msg)
 	{
 		// Transform of mocap rigid body w.r.t. world, i.e., P_world = transform*P_body
 		tf::Transform transform(tf::Quaternion(msg->pose.orientation.x,msg->pose.orientation.y,msg->pose.orientation.z,msg->pose.orientation.w),
-								tf::Vector3(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z));
+								tf::Vector3(0,0,0));
 		
 		// Transform of camera w.r.t. world, i.e., P_world = transform*P_camera
 		transform *= staticTransform_;
@@ -42,39 +44,20 @@ public:
 		// Get roll, pitch, yaw of transform
 		tfScalar roll, pitch, yaw;
 		transform.getBasis().getEulerYPR(yaw, pitch, roll);
-		std::cout << "Built in:" << std::endl;
-		std::cout << "\troll:\t" << roll << std::endl;
-		std::cout << "\tpitch:\t" << pitch << std::endl;
-		std::cout << "\tyaw:\t" << yaw << std::endl;
-		
-		tf::Quaternion quat = transform.getRotation();
-		tfScalar q1 = quat.getX();
-		tfScalar q2 = quat.getY();
-		tfScalar q3 = quat.getZ();
-		tfScalar q0 = quat.getW();
-		
-		roll = atan2(2*(q0*q1+q2*q3),1-2*(pow(q1,2)+pow(q2,2)));
-		pitch = asin(2*(q0*q2-q3*q1));
-		yaw = atan2(2*(q0*q3+q1*q2),1-2*(pow(q2,2)+pow(q3,2)));
-		
-		std::cout << "Manual:" << std::endl;
-		std::cout << "\troll:\t" << roll << std::endl;
-		std::cout << "\tpitch:\t" << pitch << std::endl;
-		std::cout << "\tyaw:\t" << yaw << std::endl;
 		
 		// Adjust transform to compensate for gimbal
-		tf::Quaternion rollQuat;
-		rollQuat.setEuler(0,0,-roll);
-		tf::Quaternion pitchQuat;
-		pitchQuat.setEuler(0,-pitch,0);
-		tf::Quaternion panTiltQuat;
-		panTiltQuat.setEuler(pan_,tilt_,0);
+		tf::Matrix3x3 rollMat;
+		rollMat.setEulerYPR(0,0,-roll);
+		tf::Matrix3x3 pitchMat;
+		pitchMat.setEulerYPR(0,-pitch,0);
+		tf::Matrix3x3 panTiltMat;
+		panTiltMat.setEulerYPR(pan_,tilt_,0);
 		
-		transform *= tf::Transform(rollQuat)*tf::Transform(pitchQuat);
-		transform *= tf::Transform(panTiltQuat);
+		tf::Transform newTransform = staticTransform_*tf::Transform(rollMat,tf::Vector3(0,0,0))*tf::Transform(pitchMat,tf::Vector3(0,0,0));
+		newTransform *= tf::Transform(panTiltMat,tf::Vector3(0,0,0));
 		
 		// Publish transform
-		br_.sendTransform(tf::StampedTransform(transform, msg->header.stamp, bebopName_, bebopImageName_));
+		br_.sendTransform(tf::StampedTransform(newTransform, msg->header.stamp, bebopName_, bebopImageName_));
 	}
 	
 	void camStateCB(const bebop_msgs::Ardrone3CameraStateOrientation::ConstPtr& msg)
